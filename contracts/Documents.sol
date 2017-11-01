4db16badf75c9d51f5a39c06001873f54b1e95a3
jharwood0 <joshuamharwood@gmail.com>
2017-11-01 15:12:35 +0000

    use structs correctly

--- contracts/Documents.sol
+++ contracts/Documents.sol
@@ -3,13 +3,20 @@ pragma solidity ^0.4.17;
 contract Documents {
   mapping (address => Person) public people;
   struct Person {
-    address _owner; //proof of ownership
+    address owner; //proof of ownership
     mapping (bytes32 => bool) documents; //proof of existence map
   }
 
   function notarize(string document) returns (bool) {
     var proof = sha256(document);
-    people[msg.sender].documents[proof] = true;
+    if(people[msg.sender].length == 0){
+      Person memory newPerson;
+      newPerson.owner = msg.sender;
+      newPerson.documents[proof] = true;
+      people[msg.sender] = newPerson;
+    }else{
+      people[msg.sender].documents[proof] = true;
+    }
     return true;
   }
 
