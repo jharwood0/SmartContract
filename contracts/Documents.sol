3d53192e7a09ac47502d7274c7989e60835bdc96
jharwood0 <joshuamharwood@gmail.com>
2017-11-01 16:56:21 +0000

    mvp of document proof of existence + ownership

--- contracts/Documents.sol
+++ contracts/Documents.sol
@@ -2,9 +2,12 @@ pragma solidity ^0.4.17;
 
 contract Documents {
   mapping (address => Person) public people;
+  address[] public addressLUT;
+
   struct Person {
     address owner; //proof of ownership
     mapping (bytes32 => bool) documents; //proof of existence map
+    bytes32[] hashesLUT;
   }
 
   function addDocument(string document) {
@@ -13,11 +16,22 @@ contract Documents {
       Person memory newPerson;
       newPerson.owner = msg.sender;
       people[msg.sender] = newPerson;
+      addressLUT.push(msg.sender);
     }
     people[msg.sender].documents[proof] = true;
+    people[msg.sender].hashesLUT.push(proof);
   }
 
   function checkDocument(string document) constant returns (bool){
     return people[msg.sender].documents[sha256(document)];
   }
+
+  function getPeople() constant returns(address[]){
+    address[] memory addresses = new address[](addressLUT.length);
+    for(uint i = 0; i < addressLUT.length; i++){
+      addresses[i] = addressLUT[i];
+    }
+    return addresses;
+  }
+
 }
